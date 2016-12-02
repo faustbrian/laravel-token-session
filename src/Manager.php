dc5cd20a220f8c73af5802c7726ea5b50cfbff11
Brian Faust <faustbrian@users.noreply.github.com>
2016-12-02 21:21:13 +0200

    Apply fixes from StyleCI (#4)
    [ci skip] [skip ci]

--- src/Manager.php
+++ src/Manager.php
@@ -46,12 +46,12 @@ class Manager
 
     public function guest()
     {
-        return ! $this->check();
+        return !$this->check();
     }
 
     public function user(array $user = [])
     {
-        if (! empty($user)) {
+        if (!empty($user)) {
             $this->manager->put('user', new Collection($user));
 
             $this->manager->save();
@@ -72,7 +72,7 @@ class Manager
 
     public function get($key, $default = null)
     {
-        if (! $this->user()->has($key)) {
+        if (!$this->user()->has($key)) {
             return false;
         }
 
